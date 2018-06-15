d192a568fa72060064bc1b037382f4d1188082dd
vadymMyroniuk <v.myroniuk@student.po.edu.pl>
2018-06-15 11:30:11 +0200

    ok

--- server/sources/main.cpp
+++ server/sources/main.cpp
@@ -14,12 +14,14 @@
 #define PORT 8080
 
 std::mutex plik;
-//std::map<>
-
-struct header{
+std::map<std::string, int> usersOnline;
+// msgId:
+// 1:register
+// 2:login
+// 3:exit
+// 
+struct msg_header{
 	unsigned int msgId;
-	unsigned int contentSize;
-	char payLoad[1];
 };
 
 
@@ -27,7 +29,8 @@ struct code{
   int codeId;
 };
 
-struct user{
+struct msg_auth{
+  msg_header header;
   char username[20];
   char password[20];
 };
@@ -39,77 +42,82 @@ int existUsername(const char* curentUsername){
 		getline(fileUsers,line);
 		char* newline = strdup(line.c_str());
 		char* usern = strtok(newline,":");
-		if (usern != NULL)
-		if (strcmp(curentUsername,usern)==0) return 1;
+		if (usern != NULL){
+			if (strcmp(curentUsername,usern)==0) {
+				fileUsers.close();
+				return 1;
+			}
+		}
 	}
+	fileUsers.close();
 	return 0;
 }
 void thread_client(int socket){
-			printf("theard_client %d",socket);
+			printf("theard_client %d\n",socket);
 			char buffer[1024];
-			while(1) {
+			char nameUser[20]="";
+			bool logged = false;
+			while(!logged) {
 				recv( socket , buffer, 1024,0);
-
-				header* headerRequest = (header*) buffer;
+				msg_auth* authRequest = (msg_auth*) buffer;
 				code codeResponse;
 				printf("I work\n");
-				if(headerRequest->msgId == 1){
-				
-
-					codeResponse.codeId = 200;
-					send(socket,&codeResponse,sizeof(code),0);
-
-					recv( socket , buffer, 1024,0);
-					user* registrationRequest = (user*) buffer;
-
+				if(authRequest->header.msgId == 1){
+					std::lock_guard<std::mutex> lock(plik);
+					if(!existUsername(authRequest->username)){
 					//zapis do pliku
-					std::ofstream fileUsers;
-					fileUsers.open("../users.txt", std::ios_base::app);
-					fileUsers<<registrationRequest->username<<":"<<registrationRequest->password<<'\n';
-					fileUsers.close();
-
-					codeResponse.codeId = 200;
-					send(socket,&codeResponse,sizeof(code),0);
+						std::ofstream fileUsers;
+						fileUsers.open("../users.txt", std::ios_base::app);
+						fileUsers<<authRequest->username<<":"<<authRequest->password<<'\n';
+						fileUsers.close();
+						codeResponse.codeId = 200;
+						send(socket,&codeResponse,sizeof(code),0);
+						logged = true;
+						strcpy(nameUser, authRequest->username);
+					}else{
+						codeResponse.codeId = 201;
+						send(socket,&codeResponse,sizeof(code),0);
+					}
 
 				}
-				else if(headerRequest->msgId == 2){
-					
-
-					codeResponse.codeId = 200;
-					send(socket,&codeResponse,sizeof(code),0);
-
-					recv( socket , buffer, 1024,0);
-					user* registrationRequest = (user*) buffer;
-
+				else if(authRequest->header.msgId == 2){
+					std::lock_guard<std::mutex> lock(plik);
 					//odczyt z pliku
 					std::ifstream fileUsers;
 					fileUsers.open("../users.txt", std::ios_base::in);
 					std::string line = "";
-					int is_user=0;
 					while(!fileUsers.eof()){
 						getline(fileUsers,line);
 						char* newline = strdup(line.c_str());
 						char* usern = strtok(newline,":");
 						char* pwd = strtok(NULL,":");
 						if (usern != NULL && pwd != NULL){
-							if (!strcmp(usern,registrationRequest->username) && !strcmp(pwd,registrationRequest->password)){ is_user = 1;
+							if (!strcmp(usern,authRequest->username) && !strcmp(pwd,authRequest->password)){ 
+							logged = true;
 							break;
 							}
 						}
 					}
 					fileUsers.close();
 
-					if (is_user) codeResponse.codeId = 200;
+					if (logged){
+						strcpy(nameUser, authRequest->username);
+						codeResponse.codeId = 200;
+						send(socket,&codeResponse,sizeof(code),0);
+					}
 					else  codeResponse.codeId = 205;
 					send(socket,&codeResponse,sizeof(code),0);
 				}
 				else {
 					printf("\nWe have a problem =(\n");
+					printf("\nauthRequest->header.msgId !=(1,2)\n");
 					close(socket);
 					break;
 				}
 
 			}
+			printf("I connected user:%s \n",nameUser); 
+			
 
 }
 
@@ -119,7 +127,6 @@ int main(int argc, char const *argv[])
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024];
 
 		//tworzenie pliku
 		std::ifstream ifile("../users.txt");
@@ -144,6 +151,8 @@ int main(int argc, char const *argv[])
     }
 
     printf("\n=>Socket server has been created...\n");
+
+    memset(&address, '0', sizeof(address));
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
@@ -155,14 +164,12 @@ int main(int argc, char const *argv[])
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 9999) < 0)
+    if (listen(server_fd, 100) < 0)
     {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-	std::cout<<"exist:"<<existUsername("tam");
 while(1){
-	printf("while\n");
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
                        (socklen_t*)&addrlen))<0)
     {
@@ -170,7 +177,6 @@ while(1){
         exit(EXIT_FAILURE);
     }
 		printf("=>Connected new client :socket_id = %d\n",new_socket);
-		
 		std::thread t(thread_client,new_socket);
 		t.detach();
 
